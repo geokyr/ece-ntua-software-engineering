2e9d1d0ad4a9ed26e52f28c0a63ae14cab13db19
geokyr <imgeorgekyr@gmail.com>
2022-02-15 00:46:44 +0200

    added main cli commands

---
+++ cli/main/passesanalysis.py
@@ -0,0 +1,24 @@
+import argparse
+import requests
+import csv
+
+my_parser = argparse.ArgumentParser(description='returns all the passes between two toll \
+    providers in a specific period with ids and period provided in the params of the url')
+my_parser.add_argument('--format', metavar='{json|csv}', type=str, 
+                        help='select the format type of the output (json or csv)')
+args = my_parser.parse_args()
+input_format = args.format
+
+r = requests.get('http://127.0.0.1:9103/admin/passesanalysis')
+data = r.json()
+
+try:    
+    if input_format == 'json':
+        print(data)
+    elif input_format == 'csv': 
+        output_file = open('../csv/passesanalysis.csv', 'w')
+        output = csv.writer(output_file)
+        output.writerow(data.keys())
+        output.writerow(data.values())
+except Exception as e:
+    print(e)
