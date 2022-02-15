9b668085327334a0aefe7552f89a868517b29b65
sertze <sertze2000@gmail.com>
2022-02-15 18:15:31 +0200

    main and admin cli commands done

--- cli/admin/healthcheck.py
+++ cli/admin/healthcheck.py
@@ -1,23 +1,17 @@
 import argparse
 import requests
-import csv
 
 my_parser = argparse.ArgumentParser(description='checks if there is connection with database')
 my_parser.add_argument('--format', metavar='{json|csv}', type=str, 
                         help='select the format type of the output (json or csv)')
 args = my_parser.parse_args()
-input_format = args.format
 
 r = requests.get('http://127.0.0.1:9103/admin/healthcheck')
-data = r.json()
 
 try:    
-    if input_format == 'json':
-        print(data)
-    elif input_format == 'csv': 
-        output_file = open('../csv/healthcheck.csv', 'w')
-        output = csv.writer(output_file)
-        output.writerow(data.keys())
-        output.writerow(data.values())
+    if args.format == 'json':
+        print(r.json())
+    elif args.format == 'csv': 
+        print(r.text)
 except Exception as e:
     print(e)
--- cli/main/chargesby.py
+++ cli/main/chargesby.py
@@ -1,24 +1,25 @@
 import argparse
 import requests
-import csv
 
 my_parser = argparse.ArgumentParser(description='returns the amount of money each visiting operator owes \
     to a specific operator in a period, with operator id and period provided in the params of the url')
+my_parser.add_argument('--op1', metavar='operator_id', type=str, 
+                        help='select the station operator')
+my_parser.add_argument('--datefrom', metavar='YYYYMMDD', type=str, 
+                        help='select the starting date')
+my_parser.add_argument('--dateto', metavar='YYYYMMDD', type=str, 
+                        help='select the ending date')
 my_parser.add_argument('--format', metavar='{json|csv}', type=str, 
                         help='select the format type of the output (json or csv)')
 args = my_parser.parse_args()
-input_format = args.format
 
-r = requests.get('http://127.0.0.1:9103/admin/chargesby')
-data = r.json()
+url = 'http://127.0.0.1:9103/ChargesBy/' + args.op1 + '/' + args.datefrom + '/' + args.dateto + '?format=' + args.format
+r = requests.get(url)
 
 try:    
-    if input_format == 'json':
-        print(data)
-    elif input_format == 'csv': 
-        output_file = open('../csv/chargesby.csv', 'w')
-        output = csv.writer(output_file)
-        output.writerow(data.keys())
-        output.writerow(data.values())
+    if args.format == 'json':
+        print(r.json())
+    elif args.format == 'csv': 
+        print(r.text)
 except Exception as e:
     print(e)
